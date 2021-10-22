962bb5829f26261fa4ae48297ee859c80cb8da0e
zeronecodes <59300820+zeronecodes@users.noreply.github.com>
2021-10-22 19:28:05 +0800

    Code update

--- smart_cart/smart_cart.ino
+++ smart_cart/smart_cart.ino
@@ -1,3 +1,6 @@
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +8,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println("This code should run infinitely");
 }
